2dc443ca53ef441793f07cc46a37cfe986919930
Datycoon <escowebs@gmail.com>
2024-02-07 17:57:44 +0000

    data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-""" Returns a salted, hashed password, byte in string """
+"""
+Hashing with bcrypt
+"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns byte string password """
+    """
+    hash password
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Implement is_valid to validate provided password
-    matched hashed_password
+    """
+    Validate password
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
